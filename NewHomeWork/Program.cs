35de8dbd3c878156de96327cf324e4f47c40bf84
nato shanshiashvili <n.shanshika@gmail.com>
2023-03-31 01:33:43 +0400

    save

--- NewHomeWork/Program.cs
+++ NewHomeWork/Program.cs
@@ -1,133 +1,103 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Xml.Linq;
 
 class HomeWork7
 {
     static void Main(string[] args)
     {
-     // FirstTask
+        MyFirstMethod();
+        Console.ReadLine();
+        Examples();
+        Params();
+        WeightParms();
 
-        Console.WriteLine("please enter number x: ");
-        int x = Int32.Parse(Console.ReadLine());
+        var result = NameSurnameAge("Nata", "Shanshiashvili", 34);
+        Console.WriteLine(result);
+        Console.ReadLine();
+    }    
+    //1
+    static void MyFirstMethod()
+    {
+        Console.Write("Enter your Name: ");
+        string FirstName = Console.ReadLine();
 
-        if (x % 2 ==0)
+        Console.Write("Enter Current Date In Format (DD/MM/YYYY): ");
+        string CurrentDate = Console.ReadLine();
+
+        Console.Write("Enter your Age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Your Name: " + FirstName + " / " + "Current Date is: " + CurrentDate + " / " + "Your Age: " + age);
+
+        Console.ReadLine();
+    }
+    //2
+    static void Examples()
+    {
+        Console.WriteLine("Please enter First Number: ");
+        int FirstNumber = Int32.Parse(Console.ReadLine());
+
+        Console.WriteLine("Please enter Second Number: ");
+        int SecondNumber = Int32.Parse (Console.ReadLine());
+
+        Console.WriteLine("Please Try Symbol + or * ");
+        string Symbol = Console.ReadLine();
+
+        if (Symbol == "+")
         {
-            Console.WriteLine("Your number is even");
+            Console.WriteLine("Your result is: "+ (FirstNumber + SecondNumber));
+            Console.ReadLine();
+        }else if (Symbol == "*")
+        {
+            Console.WriteLine("Your result is: " + (FirstNumber * SecondNumber));
+            Console.ReadLine();
         }
         else
         {
-            Console.WriteLine("Your number is odd ");
+            Console.WriteLine("Your Entered Symbol Is Wrong!");
+            Console.ReadLine();
         }
-        Console.ReadLine();
+    }
+    //3
+    static void Params()
+    {
+        Console.WriteLine("Please Enter Wish Number:");
+        int WishNumber = Int32.Parse(Console.ReadLine());
 
-     //ThecondTask
-        Console.WriteLine("Write your number to know what the day is today: ");
-        int WeekDay = Convert.ToInt32(Console.ReadLine());
-     
-      switch (WeekDay)
-            {
-            case 1:
-                Console.WriteLine("Today is Monday");
-                break;
-            case 2:
-                Console.WriteLine("Today is Tuesday");
-                break;
-            case 3:
-                Console.WriteLine("Today is Wednesday");
-                break;
-            case 4:
-                Console.WriteLine("Today is Thursday");
-                break;
-            case 5:
-                Console.WriteLine("Today is Friday");
-                break;
-            case 6:
-                Console.WriteLine("Today is Saturday");
-                break;
-            case 7:
-                Console.WriteLine("Today is Sunday");
-                break;
-            default:
-                Console.WriteLine("We have no more week days");
-                break;
-      } Console.ReadLine();
-
-     // ThirdTask
-
-        int a = 5;
-
-        while (a >= 0)
+        if (WishNumber < 0)
         {
-            Console.WriteLine("number = {0} ", a );
-            a--;
+            Console.WriteLine("Your answer is {0}");
+            Console.ReadLine();
         }
-        Console.ReadLine();
-        
-     //FourthTask
-        Console.WriteLine("Enter Your Number y:");
-        int y = Int32.Parse(Console.ReadLine());
-
-        do
+        else 
         {
-          Console.WriteLine("Number = {0}", y);
-            y--;
-        } while (y >= 0);
-          Console.ReadLine();
-
-     //FivesTask
-        Console.WriteLine("Enter Your Year of Birth: ");
-        int Date = Int32.Parse(Console.ReadLine());
-
-        do
-        {
-            Console.WriteLine("Year of Birth is = {0}", Date);
-            Date++;
-        } while (Date < 2016);
-        Console.ReadLine();
-
-     //SixthTask
-        int b = 0;
-
-        while (b <10)
-        {
-            Console.WriteLine("numebr = {0}", b);
-            b++;
+            Console.WriteLine("Your answer is: " + WishNumber);
+            Console.ReadLine();
         }
-        Console.ReadLine();
+    }
+    //4
+    static void WeightParms()
+    {
+        Console.WriteLine("Enter One Item Weight in killogramms: ");
+        double kg = double.Parse(Console.ReadLine());
 
-        //SeventhTask
+        Console.WriteLine("Your Items are 50 Pices ");
+        int pc = 50;
 
-        Console.WriteLine("Write Your First Number w: ");
-        int w = Int32.Parse(Console.ReadLine());
-
-        Console.WriteLine("Write Your Thecond Number t: ");
-        int t = Int32.Parse(Console.ReadLine());
-
-        int Fnum = 0;
-        int Snum = 0;
-
-        if (w == t)
-        {
-            Console.WriteLine("Your values are wrong");
-        }
-        if (w > t)
-        {
-            Fnum = w;
-            Snum = t;
-        }
-        if (t > w)
-        {
-            Fnum = w;
-            Snum = t;
-        }
-
-        for (int i = Fnum; i < Snum  ; i++)
-        {
-            if (i % 2 == 0)
-            {
-                Console.WriteLine(" Your Numbers is: " + i);
-            }
-        }
+        Console.WriteLine("Your transportation params is: " + (kg * pc) + "killogramms");
         Console.ReadLine();
     }
+    //5
+    
+      static string NameSurnameAge(string name, string surname)
+      {
+        return name + surname;
+      }
+      static string NameSurnameAge(string name, string surname, int age)
+      {
+         return name + surname + age;
+      }
+    
+
 }
